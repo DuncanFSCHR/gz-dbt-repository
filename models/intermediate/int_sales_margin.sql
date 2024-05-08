c11d740a454c499952edbe9edae3372419e145ec
Duncan Fischer <dncnfschr@gmail.com>
2024-05-08 13:42:04 +0000

    gz_raw_sales > sales

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,12 @@
-select *
-from {{ref('stg_gz_raw_data__raw_gz_sales')}}
-JOIN {{ref('stg_gz_raw_data__raw_gz_product')}}
-USING (product_id)
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
